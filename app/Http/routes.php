d4cff66a455f3650d4ba31d7d268f728d7eef292
Jason Curran <jc@jasoncurran.com>
2016-02-08 16:10:51 -0500

    test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -27,8 +27,11 @@ Route::get('/', function () {
 */
 
 Route::group(['middleware' => ['web']], function () {
-    
+
     // Authentication routes...
+    Route::get('auth/jc', function () {
+        return view('welcome');
+    });
     Route::get('auth/login', 'Auth\AuthController@getLogin');
     Route::post('auth/login', 'Auth\AuthController@postLogin');
     Route::get('auth/logout', 'Auth\AuthController@getLogout');
